c9365d111c1b05e0ff82eb45596bd8c890f2e786
Macias, Sebastian Patrick <sebastian.macias@stud.uni-heidelberg.de>
2025-03-16 21:32:00 +0100

    build now possible, changed a few files and dependencies to make it work, we can now build FAT JARs

---
+++ src/main/java/com/pach/gsm/App.java
@@ -0,0 +1,43 @@
+package com.pach.gsm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        supabaseAuthentication.sessionManager();
+        System.out.println("Access Token: " + storageManager.getInstance().getAccessToken());
+
+
+        String fxmlFile;
+        if (supabaseAuthentication.autoLogin()){
+            fxmlFile = "/com/pach/gsm/views/listView.fxml"; // Otherwise, show login
+            stage.setTitle("GSM");
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlFile));
+            Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+            stage.setMinWidth(1000);
+            stage.setMinHeight(600);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            fxmlFile = "/com/pach/gsm/views/loginView.fxml"; // Otherwise, show login
+            stage.setTitle("GSM - Login");
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlFile));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/pach/gsm/Launcher.java
@@ -0,0 +1,9 @@
+package com.pach.gsm;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args){
+        App.main(args);
+    }
+}
